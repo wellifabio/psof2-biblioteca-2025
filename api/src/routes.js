7978215b70b4944320b68a6a70ccee888efa620f
wellifabio <wellifabio@hotmail.com>
2025-03-31 09:20:22 -0300

    Back-end em testes

--- api/src/routes.js
+++ api/src/routes.js
@@ -9,4 +9,23 @@ routes.get('/', (req, res) => {
   return res.json({ titulo: 'Biblioteca ACME' });
 });
 
+routes.post('/alunos', Aluno.create);
+routes.get('/alunos', Aluno.read);
+routes.get('/alunos/:ra', Aluno.readOne);
+routes.patch('/alunos/:ra', Aluno.update);
+routes.delete('/alunos/:ra', Aluno.remove);
+
+routes.post('/livros', Aluno.create);
+routes.get('/livros', Livro.read);
+routes.get('/livros/:id', Livro.readOne);
+routes.patch('/livros/:id', Livro.update);
+routes.delete('/livros/:id', Livro.remove);
+
+routes.post('/emprestimos', Emprestimo.create);
+routes.get('/emprestimos', Emprestimo.read);
+routes.get('/emprestimos/:id', Emprestimo.readOne);
+routes.patch('/emprestimos/:id', Emprestimo.update);
+routes.delete('/emprestimos/:id', Emprestimo.remove);
+
+
 module.exports = routes;
